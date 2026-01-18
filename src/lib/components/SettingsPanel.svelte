32107c78993c313fcd1c0c78223f0ba162d27e8e
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-01-18 20:35:47 +0100

    style: tweak crf label

--- src/lib/components/SettingsPanel.svelte
+++ src/lib/components/SettingsPanel.svelte
@@ -102,7 +102,7 @@
                     >Quality Factor</label
                 >
                 <div
-                    class="text-[10px] bg-ds-gray-100 px-1.5 rounded text-black font-bold"
+                    class="text-[10px] bg-ds-blue-900/20 text-ds-blue-600 border border-ds-blue-600 font-mono px-1.5 rounded font-bold"
                 >
                     CRF {config.crf}
                 </div>
